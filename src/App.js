a4824ce14d838ad3d031d30658032c1695e41d6c
Yvonne Huang <huangq9@uw.edu>
2021-03-06 09:28:31 -0800

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [isDrawing, setIsDrawing] = useState(false)
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width  = 500;
+    canvas.height = 500; 
+    // canvas.width = window.innerWidth ;
+    // canvas.height = window.innerHeight *2;
+    // canvas.style.width = `${window.innerWidth}px`;
+    // canvas.style.height = `${window.innerHeight}px`;
+    document.getElementById('reset').addEventListener('click', function() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }, false);
+
+    const context = canvas.getContext("2d")
+    // context.scale(2, 2);
+    context.lineCap = "round";
+    const colors = ['red', 'blue', 'green', 'purple', 'yellow', 'orange', 'pink', 'black', 'white'];
+    function listener(i) {
+      document.getElementById(colors[i]).addEventListener('click', function() {
+        context.strokeStyle = colors[i];
+      }, false);
+    }
+    for(var i = 0; i < colors.length; i++) {
+      listener(i);
+    }
+    context.lineWidth = 5;
+    contextRef.current = context;
+  },[]);
+
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const endDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "drawing-panel">
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
     </div>
   );
 }
+
+  
 
 export default App;

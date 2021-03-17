4008c4ff265ac8e21e5608e43d102ad8f11381c9
Yvonne Huang <huangq9@uw.edu>
2021-03-16 20:09:57 -0700

    more changes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   const [canvasOffset, setCanvasOffset] = useState ({x:0,y:0});
   const [tool, setTool] = useState('pen')
   const [strokeColor, setStrokeColor] = useState ()
+
  
   useEffect(()=>{
     const canvas = canvasRef.current;  
@@ -22,7 +23,7 @@ function App() {
     canvas.width = parentRef.current.offsetWidth;
     canvas.height = parentRef.current.offsetHeight;
     cavctx.lineCap = "round";
-    cavctx.lineWidth = 2;
+    cavctx.lineWidth = 3;
     cavctx.scale(2,2)
     setContext(cavctx);
     document.getElementById('clear').addEventListener('click', function() {
@@ -56,7 +57,6 @@ function App() {
         context.lineTo(mouseX,mouseY);
         context.stroke();
     } else {
-        setTool ('eraser')  
         context.strokeStyle = 'white';
         context.beginPath();
         context.moveTo(position.x, position.y);
@@ -84,8 +84,10 @@ function App() {
         ref={canvasRef}
       />
 
-      <div className='tools'>
-        <Tools />
+      <div className='tools' >
+
+        <Tools setTool={setTool}/>
+
 
       </div>
    
--- src/Tools.js
+++ src/Tools.js
@@ -1,56 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SketchPicker } from 'react-color';
 import './App.css';
 
-// function Tools() {
-  
-    
-//     return (
-//         <div className ='tool-bar' >
-    
-//         <div id='eraser' className="tool" ></div>
-//         <div id='clear' className="tool"></div>
-//         <div id='palette' className="tool"></div>
-//         <div id='save' className="tool"></div>
+function Tools({setTool}) {
 
-//         </div>
-
-//     );
-// }
-
-// export default Tools;
-
-class Tools extends React.Component {
-
-        // state = {
-        //     background: '#fff',
-        // };
-       
-        // handleChangeComplete = (color) => {
-        //     this.setState({ background: color.hex });
-        // };
-
-    render() { 
-    
-    
-        return (
-            <div>
-            <div className ='tool-bar' >
-    
-            <div id='eraser' className="tool" ></div>
-            <div id='clear' className="tool" ></div>
-            <div id='palette' className="tool" ></div>
-            <div id='save' className="tool"></div>
-            
-            </div>
-            
-            {/* <SketchPicker/> */}
-        </div>
-        )
+    function Eraser(){
+      setTool ('eraser')
     }
+
+    function Palette (){
+        setTool ('pen')
+    }
+    
+    return (
+        <div className ='tool-bar' >
+    
+        <div id='eraser' className="tool" onClick = {Eraser}></div>
+        <div id='clear' className="tool" onChange ></div>
+        <div id='palette' className="tool" onClick ={Palette}></div>
+        <div id='save' className="tool"></div>
+
+        </div>
+
+    );
 }
 
 export default Tools;
+
+// class Tools extends React.Component {
+
+//         // state = {
+//         //     background: '#fff',
+//         // };
+       
+//         // handleChangeComplete = (color) => {
+//         //     this.setState({ background: color.hex });
+//         // };
+
+//     render() { 
+//         return (
+//             <div>
+//             <div className ='tool-bar' >
+    
+//             <div id='eraser' className="tool" ></div>
+//             <div id='clear' className="tool" ></div>
+//             <div id='palette' className="tool" ></div>
+//             <div id='save' className="tool"></div>
+            
+//             </div>
+            
+//             {/* <SketchPicker/> */}
+//         </div>
+//         )
+//     }
+// }
+
+// export default Tools;
 
 
 
